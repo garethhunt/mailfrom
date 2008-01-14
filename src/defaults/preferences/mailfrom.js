272554951059016d363f27398e1470ae6f318872
gareth <gareth>
2008-01-14 03:16:25 +0000

    Updated name of default.  Fixed URL for yahoo mail

--- src/defaults/preferences/mailfrom.js
+++ src/defaults/preferences/mailfrom.js
@@ -4,7 +4,7 @@ pref("extensions.mailfrom.open.in", 2);
 //pref("extensions.mailfrom.services", "default,gmail,windowslive,yahoomail");
 pref("extensions.mailfrom.services", "default,gmail,yahoomail");
 pref("extensions.mailfrom.service.default.enabled", true);
-pref("extensions.mailfrom.service.default.name", "default");
+pref("extensions.mailfrom.service.default.name", "System Mail Client");
 pref("extensions.mailfrom.service.default.url", "");
 pref("extensions.mailfrom.service.gmail.enabled", true);
 pref("extensions.mailfrom.service.gmail.name", "GMail");
@@ -15,4 +15,4 @@ pref("extensions.mailfrom.service.gmail.url", "https://mail.google.com/mail/?vie
 //pref("extensions.mailfrom.service.windowslive.url", "https://mail.live.com/");
 pref("extensions.mailfrom.service.yahoomail.enabled", true);
 pref("extensions.mailfrom.service.yahoomail.name", "Yahoo Mail");
-pref("extensions.mailfrom.service.yahoomail.url", "https://compose.mail.yahoo.com/?to=$TO");
+pref("extensions.mailfrom.service.yahoomail.url", "http://compose.mail.yahoo.com/?To=$TO");
